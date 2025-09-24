9924557baf0a17fc919a22af9016826e971db306
sduddla <sduddla921@gmail.com>
2025-09-25 03:04:42 +0900

    Feat: 새로고침해도 할 일 유지되도록 처리

--- vue/src/stores/todo.js
+++ vue/src/stores/todo.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 
 export const useTodoStore = defineStore('todo', () => {
   const todos = ref([])
@@ -19,6 +19,24 @@ export const useTodoStore = defineStore('todo', () => {
   const deleteTodo = (id) => {
     todos.value = todos.value.filter((todo) => todo.id !== id)
   }
+
+  // 저장
+  watch(
+    todos,
+    (newTodos) => {
+      sessionStorage.setItem('todos', JSON.stringify(newTodos))
+    },
+    { deep: true },
+  )
+
+  // 복원
+  onMounted(() => {
+    const saved = sessionStorage.getItem('todos')
+
+    if (saved) {
+      todos.value = JSON.parse(saved)
+    }
+  })
 
   return {
     todos,
